868ccf4ccbef2cd099d421edbe06ca2536a3a358
Archana Tyagi <79385393+archana523@users.noreply.github.com>
2021-10-04 17:12:44 +0100

    Add files via upload

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -23,6 +23,8 @@ var helloPopup;
 var popup;
 var imageUrl;
 var url;
+var flag;
+var easy_button;
 $(document).ready(function(){
 	$('#submitWeather').click(function() {
 			$.ajax({
@@ -52,9 +54,10 @@ $(document).ready(function(){
 					$('#area').html(result['data']['area']);
 					$('#lang').html(result['data']['languages'][0]['name']);
 					$('#nat').html(result['data']['nativeName']);
-					$('#con').html(result['data']['continent']);
+					$('#sub').html(result['data']['subregion']);
 					$('#tim').html(result['data']['timezones']);
-					$('#flag').html(result['data']['flags'][0]);
+					flag = result['data']['flag'];
+					$('#flag').html(`<img src='${flag}' width="40" height="40">`);
 					console.log('hey');
 					//POLYGON USING COUNTRY BORDER COORDINATES.
 					$.getJSON("libs/js/countryBorders.geo.json", function(result) {
@@ -75,6 +78,7 @@ $(document).ready(function(){
 					//ADDING MARKER TO MAP
 					if (marker != null) mymap.removeLayer(marker);
 					marker = L.marker(result['data']['latlng']).addTo(mymap);
+					marker.bindPopup(L.popup().setContent(result['data']['capital']));
 					//FLAG IMAGE ON MAP
 					//url = result['data']['flags'][0];
 					//console.log(url);
@@ -97,13 +101,22 @@ $(document).ready(function(){
 							console.log(JSON.stringify(data1));
 								if (result.status.name == "ok"){
 									console.log(data1['data']['weather'][0]['description']);
-									popup = L.popup().setLatLng(result['data']['latlng'])
-    .setContent('<h4>Weather report</h4>'+ 'Clouds: '+data1['data']['weather'][0]['description']
-				+'<br />'+'Temp: '+ data1['data']['main']['temp']+'<br />'+'Pressure: '+
-				data1['data']['main']['pressure']+'<br />'+'Humidity: '+data1['data']['main']['humidity']
-				+'<br />'+'Sea Level: '+data1['data']['main']['sea_level']+'<br />'+
-				'Latitude: '+data1['data']['coord']['lon']+'<br />'+'Longitude: '+data1['data']['coord']['lat']);
-									marker.bindPopup(popup);
+									if(popup != null) mymap.removeControl(easy_button);
+									popup = L.popup().setContent('<h4>Weather report</h4>'+ 'Clouds: '+data1['data']['weather'][0]['description']
+				+'<br />'+'Temp: '+ data1['data']['main']['temp']+' Celsius'+'<br />'+'Pressure: '+
+				data1['data']['main']['pressure']+' hPa'+'<br />'+'Humidity: '+data1['data']['main']['humidity']
+				+' %'+'<br />'+'Sea Level: '+data1['data']['main']['sea_level']+' hPa'+'<br />'+
+				'Latitude: '+data1['data']['coord']['lon']+'<br />'+'Longitude: '+data1['data']['coord']['lat']
+				+'<br />'+'Wind speed: '+data1['data']['wind']['speed']+' meter/sec');
+									//marker.bindPopup(popup);
+									//helloPopup = L.popup().setContent('<div id="openweathermap-widget-15"></div>');
+									
+									easy_button = L.easyButton('fa-globe', function(btn, map){
+										//win =  L.control.window(mymap,{position: 'center',modal: true, content:'<div id="openweathermap-widget-15"></div>'})
+									   //.showOn(0,200)
+									   //window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  window.myWidgetParam.push({id: 15,cityid: '2643743',appid: '4a953b341d84c4765dfb3d2d76c96cfb',units: 'metric',containerid: 'openweathermap-widget-15',  });  (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();
+										popup.setLatLng(mymap.getCenter()).openOn(mymap);
+									}).addTo(mymap);
 								}
 							}
 					});
@@ -116,8 +129,18 @@ $(document).ready(function(){
 			}
 		}); 
 	});
-	var mymap = L.map('mapid').locate({setView: true, maxZoom: 5});
+	var mymap = L.map('mapid').fitWorld().locate({setView: true, maxZoom: 10});
 	Esri_NatGeoWorldMap.addTo(mymap);
+	function onLocationFound(e) {
+		var radius = e.accuracy;
+	
+		L.marker(e.latlng).addTo(mymap)
+			.bindPopup("You are within " + radius + " meters from this point").openPopup();
+	
+		L.circle(e.latlng, radius+20).addTo(mymap);
+	}
+	
+	mymap.on('locationfound', onLocationFound);
 	
 	var abc;
 	if (navigator.geolocation) {
@@ -140,7 +163,8 @@ $(document).ready(function(){
 	L.easyButton('fa-exchange', function(){
 	sidebar.toggle();
 	}).addTo(mymap);
-	L.control.scale().addTo(mymap);
+	//L.control.scale().addTo(mymap);
+	
 		//ADDING BASEMAPS
 		baseMaps = {
 		"OpenStreetMap_Mapnik": OpenStreetMap_Mapnik,
@@ -148,15 +172,15 @@ $(document).ready(function(){
 		"CartoDB_Voyager": CartoDB_Voyager,
 		"Esri_NatGeoWorldMap": Esri_NatGeoWorldMap,
 		"USGS_USImageryTopo": USGS_USImageryTopo,
+		"NASAGIBS": NASAGIBS_ViirsEarthAtNight2012
 		};
 		baseLayers = {
-		"precipitationcls": precipitationcls,
-		"cloudscls": cloudscls,
+		"OpenRailwayMap": OpenRailwayMap,
 		"raincls": raincls,
 		"temp": temp,
-		"wind": wind,
-		"snow": snow
+		"wind": wind
 		};
+		//mymap.addLayer(temp);
 		//if(layerControl != null) mymap.removeControl(layerControl);
 		layerControl = L.control.layers(baseMaps, baseLayers).addTo(mymap);
 	//if(full != null) mymap.removeControl(full);
@@ -170,12 +194,13 @@ $(document).ready(function(){
 		fullscreenElement: false // Dom element to render in full screen, false by default, fallback to map._container
 	  }).addTo(mymap);
 
-	  //	helloPopup = L.popup().setContent('Hello World!');
+	  	//helloPopup = L.popup().setContent('<div id="openweathermap-widget-15"></div>');
 
 		//L.easyButton('fa-globe', function(btn, map){
-		//	win =  L.control.window(mymap,{title:'Hello world!',position: 'center', content:'This is my first control window.'})
-          // .show()
-			//helloPopup.setLatLng(map.getCenter()).openOn(map);
+			//win =  L.control.window(mymap,{position: 'center',modal: true, content:'<div id="openweathermap-widget-15"></div>'})
+           //.showOn(0,200)
+		  // window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  window.myWidgetParam.push({id: 15,cityid: '2643743',appid: '4a953b341d84c4765dfb3d2d76c96cfb',units: 'metric',containerid: 'openweathermap-widget-15',  });  (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();
+			//helloPopup.setLatLng(mymap.getCenter()).openOn(mymap);
 		//}).addTo(mymap);
 
 });
